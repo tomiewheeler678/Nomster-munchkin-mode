6c3188d0a277a1e3c25dfcaa62e7585341e4880a
tomiewheeler678 <tomiewheeler678@gmail.com>
2019-06-20 01:34:31 +0000

    nomster3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,10 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.paginate(:page params[:page], per_page: 3)
+    @page = Place.paginate(page: params[:page], per_page: 3)
   end
 
   def new
+    @place = Place.new
   end
 
 end
