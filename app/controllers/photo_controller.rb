2823010b8bbb9624782de86f88ae401d52e42c63
tomiewheeler678 <tomiewheeler678@gmail.com>
2019-06-29 17:53:19 +0000

    nomster photo

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -5,9 +5,7 @@ class PhotosController < ApplicationController
     @place = Place.find(params[:place_id])
      @place.photos.create(photo_params.merge(user: current_user))
       redirect_to place_path(@place)
-
-
-  end
+    end
 
 private
 
